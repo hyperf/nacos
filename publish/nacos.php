7c208bbd95f2f7ed82f68cdf5d6550806fb738b4
xuzhen <xuzhen@corpvip.com>
2021-07-22 19:32:20 +0800

    fix publish nacos

--- publish/nacos.php
+++ publish/nacos.php
@@ -10,15 +10,21 @@ declare(strict_types=1);
  * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
  */
 return [
-    // nacos server url like https://nacos.hyperf.io, Priority is higher than host:port
-    // 'url' => '',
-    // The nacos host info
-    'host' => '127.0.0.1',
-    'port' => 8848,
-    // The nacos account info
-    'username' => null,
-    'password' => null,
-    'guzzle' => [
-        'config' => null,
+    'service' => [
+        // nacos server url like https://nacos.hyperf.io, Priority is higher than host:port
+        // 'url' => '',
+        // The nacos host info
+        'host' => '127.0.0.1',
+        'port' => 8848,
+        'service_name' => 'service01',
+        // The nacos account info
+        'username' => null,
+        'password' => null,
+        'guzzle' => [
+            'config' => null,
+        ],
+        'instance' => [
+            'heartbeat' => 5
+        ],
     ],
 ];
